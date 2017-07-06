37c9daaa3844e4ac7447e0b338958637b318e3e2
WiktorNetSuite <wiktor.michalski000@gmail.com>
2017-07-06 14:41:16 -0300

    implemented netsuite service

---
+++ PHPToolkit/NSconfig.php
@@ -0,0 +1,10 @@
+<?php
+define("NS_ENDPOINT", "2017_1");
+define("NS_HOST", "https://webservices.netsuite.com");
+define("NS_EMAIL", "gavinm@acenda.com");
+define("NS_PASSWORD", "Vk%tGt!7zb");
+define("NS_ROLE", "3");
+define("NS_ACCOUNT", "TSTDRV1709430");
+
+define("NS_APPID", "4AD027CA-88B3-46EC-9D3E-41C6E6A325E2");
+?>
